48d8f7ed2618fdd9b3304859dc3cc31e7cac71ca
Nelaka Dilshan <nelakad@gmail.com>
2019-08-10 05:28:44 +0530

    new food, food menu

---
+++ DailyBread/app/src/main/java/com/example/dailybread/Food_Menu.java
@@ -0,0 +1,14 @@
+package com.example.dailybread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Food_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food__menu);
+    }
+}
